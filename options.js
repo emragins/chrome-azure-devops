767ad7b619e0650c63805570ade172f65f9a7711
emragins <emragins@gmail.com>
2024-08-09 19:25:02 -0700

    allow for custom urls (as for on-premises solutions (#5)

--- options.js
+++ options.js
@@ -5,9 +5,20 @@ let closeButton = document.getElementById('close');
 let input_company = document.getElementById('company');
 let input_project = document.getElementById('projectName');
 let input_newTab = document.getElementById('newTab');
+let input_customUrl = document.getElementById('customUrl');
 
 
 // setup triggers
+
+// when customUrl is selected intentionally, clear the other options and select custom
+input_customUrl.onclick = function () {
+    var urlOptions = document.getElementsByName("azure_url");
+    if (urlOptions) {
+        urlOptions[0].checked = false;
+        urlOptions[1].checked = false;
+        urlOptions[2].checked = true;
+    }
+}
 
 saveButton.onclick = function () {
 
@@ -15,22 +26,28 @@ saveButton.onclick = function () {
     var urlOptions = document.getElementsByName("azure_url");
     if (urlOptions) {
         for (var i = 0; i < urlOptions.length; i++) {
-            if (urlOptions[i].checked){
+            if (urlOptions[i].checked) {
                 url = urlOptions[i].value;
             }
         }
+    }
+
+    // trim trailing / from customUrl
+    if (url == 'custom') {
+        input_customUrl.value = input_customUrl.value.replace(/\/$/, '');
     }
 
     let options = {
         company: input_company.value,
         projectName: input_project.value,
         newTab: input_newTab.checked,
+        customUrl: input_customUrl.value,
         url: url,
     };
     // console.debug('options: ' + JSON.stringify(options));
 
     chrome.storage.sync.set(options);
-    
+
     alert('Settings saved');
 }
 
@@ -42,14 +59,14 @@ closeButton.onclick = function () {
 }
 
 
-chrome.storage.sync.get({ company: '', projectName: '', newTab: true, url: 'azure'},
+chrome.storage.sync.get({ company: '', projectName: '', newTab: true, url: 'azure', customUrl: '' },
     function (items) {
         // console.debug('newTab: ' + items.newTab);
 
         var urlOptions = document.getElementsByName("azure_url");
         if (urlOptions) {
             for (var i = 0; i < urlOptions.length; i++) {
-                if (urlOptions[i].value == items.url){
+                if (urlOptions[i].value == items.url) {
                     urlOptions[i].checked = true;
                 }
             }
@@ -58,6 +75,7 @@ chrome.storage.sync.get({ company: '', projectName: '', newTab: true, url: 'azur
         input_company.value = items.company;
         input_project.value = items.projectName;
         input_newTab.checked = items.newTab;
+        input_customUrl.value = items.customUrl;
     });
 
 
